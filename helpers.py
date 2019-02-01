4a60b5602c890f0da93eb311a251428b30c3bdde
James Langbein <james.langbein@protonmail.com>
2019-02-02 07:55:55 +1300

    Added support for generating seeds from other seeds.

--- helpers.py
+++ helpers.py
@@ -12,32 +12,51 @@ def string_shuffler(seed):
     return res
 
 
-def seed_generator(string, n):
+def seed_generator(inp, n):
 
     def seed_preprocessor(arg):
-        chars = string_shuffler(6835525)
-        arg_num_list = []
-        for char in arg:
-            if char == '"':
-                ch = '/'
-                num = chars.index(ch)
-                arg_num_list.append(str(num))
-            elif char == '\\':
-                ch = '/'
-                num = chars.index(ch)
-                arg_num_list.append(str(num))
-            else:
-                num = chars.index(char)
-                arg_num_list.append(str(num))
-        pre_num = ''.join(arg_num_list)  # returns a string of digits from arg
-        pre_num = int(pre_num)
+        if type(arg) == str:
+            chars = string_shuffler(6835525)
+            arg_num_list = []
+            for char in arg:
+                if char == '"':
+                    ch = '/'
+                    num = chars.index(ch)
+                    arg_num_list.append(str(num))
+                elif char == '\\':
+                    ch = '/'
+                    num = chars.index(ch)
+                    arg_num_list.append(str(num))
+                else:
+                    num = chars.index(char)
+                    arg_num_list.append(str(num))
+            pre_num = ''.join(arg_num_list)  # returns a string of digits from arg
+            pre_num = int(pre_num)
+        elif type(arg) == int:  # shuffle chars_list based on arg and take first 16 chars
+            chars = string_shuffler(arg)
+            f16 = chars[:15]
+            arg_num_list = []
+            for char in f16:
+                if char == '"':
+                    ch = '/'
+                    num = CHARS_LIST.index(ch)
+                    arg_num_list.append(str(num))
+                elif char == '\\':
+                    ch = '/'
+                    num = CHARS_LIST.index(ch)
+                    arg_num_list.append(str(num))
+                else:
+                    num = CHARS_LIST.index(char)
+                    arg_num_list.append(str(num))
+            pre_num = ''.join(arg_num_list)  # returns a string of digits from arg
+            pre_num = int(pre_num)
 
         # scale final_num down... using random.seed
         random.seed(pre_num)
         final_num = random.randint(0, 999999999)
         return final_num
 
-    numpy.random.seed(seed_preprocessor(string))
+    numpy.random.seed(seed_preprocessor(inp))
     res = []
     for i in range(n):
         res.append(numpy.random.randint(0, 999999999))
