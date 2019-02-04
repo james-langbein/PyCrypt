76880a024e2215dcbafccb4802c4bf208f53638b
James Langbein <james.langbein@protonmail.com>
2019-02-04 17:50:34 +1300

    Added decryption support for the new 'delimiter' function. Modified each of the 'decrypt' methods.

--- decryption.py
+++ decryption.py
@@ -52,8 +52,8 @@ class Decryptor:
         return original_message
 
     @staticmethod
-    def de_pad(string):
-        sep = '     '
+    def de_pad(string, delim_seed):
+        sep = helpers.delimiter(delim_seed)
         remaining = string.split(sep, 1)[0]
 
         return remaining
@@ -61,20 +61,20 @@ class Decryptor:
     @staticmethod
     def decrypt1(message, seed):
         """Decrypt a given message string with a given password string (seed)."""
-        x = helpers.seed_generator(seed, 3)
-        res = Decryptor.de_shuffle(message, x[2])
-        res2 = Decryptor.de_shift(res, x[0], x[1])
-        res3 = Decryptor.de_pad(res2)
+        x = helpers.seed_generator(seed, 4)
+        res = Decryptor.de_shuffle(message, x[3])
+        res2 = Decryptor.de_shift(res, x[1], x[2])
+        res3 = Decryptor.de_pad(res2, x[0])
 
         return res3
 
     @staticmethod
     def decrypt2(message, seed):
         """Decrypt a given message string with a given password string (seed)."""
-        x = helpers.seed_generator(seed, n=5)
-        res = Decryptor.de_shuffle(message, x[4])
-        res2 = Decryptor.de_shift(res, x[2], x[3])
-        res3 = Decryptor.de_pad(res2)
+        x = helpers.seed_generator(seed, n=6)
+        res = Decryptor.de_shuffle(message, x[5])
+        res2 = Decryptor.de_shift(res, x[3], x[4])
+        res3 = Decryptor.de_pad(res2, x[2])
         res4 = Decryptor.de_shift(res3, x[0], x[1])
         res5 = Decryptor.de_split(res4)
 
@@ -85,7 +85,7 @@ class Decryptor:
         d = Decryptor()
 
         seeds = helpers.seed_generator(str(seed), n=2)
-        x1 = helpers.seed_generator(seeds[0], n=2)
+        x1 = helpers.seed_generator(seeds[0], n=3)
         x2 = helpers.seed_generator(seeds[1], n=rounds)
 
         """Decrypt rounds"""
@@ -96,7 +96,8 @@ class Decryptor:
             r_res = d.de_shuffle(string=r_res, seed=r_seed[2])
             r_res = d.de_shift(string=r_res, list_seed=r_seed[0], msg_seed=r_seed[1])
 
-        res = d.de_pad(r_res)
+        """Decrypt initial obfuscation"""
+        res = d.de_pad(r_res, delim_seed=x1[2])
         res2 = d.de_shift(res, x1[0], x1[1])
         res3 = d.de_split(res2)
 
